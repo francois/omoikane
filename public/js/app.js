379e09d1c4e532e36faa01f19272ca9206cec4ab
François Beausoleil <francois@teksol.info>
2014-09-22 21:10:08 -0400

    Use consistent naming throughout

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
-App = Ember.Application.create();
+Omoikane = Ember.Application.create();
 
-App.Router.map(function() {
+Omoikane.Router.map(function() {
   // put your routes here
 });
 
-App.IndexRoute = Ember.Route.extend({
+Omoikane.IndexRoute = Ember.Route.extend({
   model: function() {
     return ['red', 'yellow', 'blue'];
   }
