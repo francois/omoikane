9770f7d15a52ba89b994d7910327b275edd02eef
François Beausoleil <francois@teksol.info>
2015-01-05 16:08:53 +0000

    Sometimes, a bit of quiet can be beneficial

--- lib/omoikane/jobsdb.rb
+++ lib/omoikane/jobsdb.rb
@@ -4,5 +4,7 @@ require "omoikane/logging"
 jobsdb_url = ENV["OMOIKANE_DATABASE_URL"]
 raise "Missing OMOIKANE_DATABASE_URL environment variable: can't connect to internal Omoikane database" unless jobsdb_url
 
-DB = Sequel.connect(jobsdb_url, logger: logger)
+ENV["QUIET"] = "yes" if ARGV.delete("--quiet")
+
+DB = Sequel.connect(jobsdb_url, logger: ENV["QUIET"] == "yes" ? nil : logger)
 Sequel::Model.plugin :tactical_eager_loading
--- lib/omoikane/targetdb.rb
+++ lib/omoikane/targetdb.rb
@@ -3,4 +3,6 @@ require "omoikane/logging"
 targetdb_url = ENV["OMOIKANE_TARGET_URL"]
 raise "Missing OMOIKANE_TARGET_URL environment variable: can't connect to target Omoikane database" unless targetdb_url
 
-TARGETDB = Sequel.connect(targetdb_url, logger: logger)
+ENV["QUIET"] = "yes" if ARGV.delete("--quiet")
+
+TARGETDB = Sequel.connect(targetdb_url, logger: ENV["QUIET"] == "yes" ? nil : logger)
